ec3b9a880847c6b8ad7853071c5e47a9ab0d249d
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2024-02-28 19:59:14 -0300

    :fire: Remove Docker container check in database_S203.bat

--- Database/database_S203.bat
+++ Database/database_S203.bat
@@ -1,12 +1,5 @@
 @echo off
 
-REM Check if Docker is running
-sc query com.docker.service | findstr "RUNNING"
-IF ERRORLEVEL 1 (
-    REM Start Docker
-    net start com.docker.service
-)
-
 REM Check if the Postgres image is already present
 docker images | findstr "postgres"
 IF ERRORLEVEL 1 (
@@ -15,7 +8,7 @@ IF ERRORLEVEL 1 (
 )
 
 REM Check if the Docker container is already running
-docker ps | findstr "database-to-S203"
+docker ps -a | findstr "database-to-S203"
 IF ERRORLEVEL 1 (
     REM Create and start the Docker container
     docker run --name database-to-S203 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -d postgres
